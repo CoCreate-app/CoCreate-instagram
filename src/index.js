bfab3ffa9f689755ed016026941a31eb7cae04a7
frank <frank@cocreate.app>
2021-05-21 03:17:30 +0000

    clean tree

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-
-const CoCreateInstagram = {
-	id: 'instagram',
-	actions: [
-		'getUserProfile'
-	],
-
-	render_getUserProfile: function(data) {
-		console.log(data)
-	}
-};
-
-api.init({
-	name: CoCreateInstagram.id, 
-	module:	CoCreateInstagram,
-});
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(["./client"], function(CoCreateInstagram) {
+        	return factory(CoCreateInstagram)
+        });
+    } else if (typeof module === 'object' && module.exports) {
+      const CoCreateInstagram = require("./server.js")
+      module.exports = factory(CoCreateInstagram);
+    } else {
+        root.returnExports = factory(root["./client.js"]);
+  }
+}(typeof self !== 'undefined' ? self : this, function (CoCreateInstagram) {
+  return CoCreateInstagram;
+}));
